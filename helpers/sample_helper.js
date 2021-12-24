65f7185b86964d39fcd4f6b9301e31897416bbe1
hvulinh <work.linhhv@gmail.com>
2021-12-24 07:21:25 +0700

    Add documentation for functions

--- helpers/sample_helper.js
+++ helpers/sample_helper.js
@@ -1,14 +1,18 @@
-import { Selector, t } from 'testcafe';
-
 class DateTime {
 	
-	async convertStringToDateTime(dateTimeStr) {
-		let year = new Date().getFullYear();    // Get current year
-		let month = dateTimeStr.toString().substring(0, 3);
-		let day = dateTimeStr.toString().substring(4, 6);
-		let hour = dateTimeStr.toString().substring(8, 10);
-		let minute = dateTimeStr.toString().substring(11, 13);
-		let dayNight = dateTimeStr.toString().substring(13);
+	/**
+	 * Convert a string into Date object.
+	 * The format of the string must be: "MMM dd, hh:mmam/pm" (e.g. Dec 23, 01:20pm).
+	 * @param {string} dateStr - The target string that will be convert to date
+	 * @return {Date} - The converted Date
+	 */
+	async convertStringToDate(dateStr) {
+		let year = new Date().getFullYear();                  // Get current year
+		let month = dateStr.toString().substring(0, 3);       // Get month
+		let day = dateStr.toString().substring(4, 6);         // Get day
+		let hour = dateStr.toString().substring(8, 10);       // Get hour
+		let minute = dateStr.toString().substring(11, 13);    // Get minute
+		let dayNight = dateStr.toString().substring(13);      // Get am/pm
 		let string = year + "-" + month + "-" + day + " " + hour + ":" + minute;
 		
 		if (dayNight == "pm") {    // Move time forward 12 hours if pm
@@ -18,7 +22,14 @@ class DateTime {
 		}
 	}
 	
-	async isDateTimeEqual(date01, date02, offset = (1 * 60 * 1000)) {
+	/**
+	 * Compare 2 dates and verify whether they are equal or not relatively using an offset value.
+	 * @param {date} date01 - The first target date to compare
+	 * @param {date} date02 - The second target date to compare
+	 * @param {number} offset - The offset value (milisecond)
+	 * @return {boolean} - The comparing result: true if equal, otherwise false
+	 */
+	async checkDateEqual(date01, date02, offset = (1 * 60 * 1000)) {
 		let duration = date01.getTime() - date02.getTime();
 		
 		if (Math.abs(duration) <= offset) {
--- page_model/home_page.js
+++ page_model/home_page.js
@@ -12,11 +12,16 @@ class HomePage {
 	    this.tempInfo = Selector('.current-temp span');
 	}
 	
+	/**
+	 * Perform the searching action for a city.
+	 * @param {string} city - The target city
+	 * @return {void} - Nothing
+	 */
 	async searchCity (city) {
 		await t
 		    .typeText(this.searchInput, city)
 		    .click(this.searchBtn)
-		    .click('.search-container ul li:nth-child(1)');    // TODO: Avoid hard-coding
+		    .click('.search-container ul li:nth-child(1)');    // TODO: Improve this locator
 	}
 }
 
